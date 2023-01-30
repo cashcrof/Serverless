2761ae13c8edf5602df617f07ef76528da1adba4
Ceilidh Ashcroft <cashcroft1@my.bcit.ca>
2023-01-29 23:11:08 -0800

    added s3 bucket upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const database = require("./database");
+const s3 = require("./s3");
 const fs = require("fs");
 const multer = require("multer");
 const storage = multer.memoryStorage()
